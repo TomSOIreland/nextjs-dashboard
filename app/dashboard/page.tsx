e115363a2ba26babd72fca10e67642e5638a5952
Tom Malkiewicz <tom.malkiewicz@specialolympics.ie>
2025-02-07 23:33:48 +0000

    Dashboard beg

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,12 @@
+import { Card } from "../ui/dashboard/cards"
+import RevenueChart from "../ui/dashboard/revenue-chart"
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
+
+
 export default function Page() {
-    return <p>Dashboard Page</p>
+    return (
+        <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+    )
 }
